15e1a6821ed364e2d6bc575be0d55c5de2d7821e
phoenixsnpd <phoenix.snpd@gmail.com>
2022-08-22 23:20:26 +0300

    Created 3 classes Animal, Dog, Cat and counter for them.

---
+++ ua.ithillel.lesson8.homework/src/ua/ithillel/homework/Cat.java
@@ -0,0 +1,25 @@
+package ua.ithillel.homework;
+
+public class Cat extends Animal {
+    private static int catCounter;
+    public Cat() {
+        catCounter++;
+    }
+
+    @Override
+    public void run(int distance) {
+        if (distance > 200) {
+            System.out.println("Too long distance");
+        } else {
+            System.out.println("The cat ran " + distance + " meters.");
+        }
+    }
+    @Override
+    public void swim(int distance) {
+        System.out.println("The cat can't swim");
+    }
+
+    public static int getCatCounter() {
+        return catCounter;
+    }
+}
