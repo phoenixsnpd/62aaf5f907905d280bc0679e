810bc7c965a0266570ffafd8f17855a91ce241ee
phoenixsnpd <phoenix.snpd@gmail.com>
2022-09-11 18:42:56 +0300

    Completed homework 10.

---
+++ ua.ithillel.homework/src/ua/ithillel/lesson10/ArrayDataException.java
@@ -0,0 +1,22 @@
+package ua.ithillel.lesson10;
+
+public class ArrayDataException extends Exception {
+    public ArrayDataException() {
+    }
+
+    public ArrayDataException(String message) {
+        super(message);
+    }
+
+    public ArrayDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ArrayDataException(Throwable cause) {
+        super(cause);
+    }
+
+    public ArrayDataException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
---
+++ ua.ithillel.homework/src/ua/ithillel/lesson10/TestApplication.java
@@ -0,0 +1,15 @@
+package ua.ithillel.lesson10;
+
+public class TestApplication {
+    public static void main(String[] args) {
+        String[][] array = {{"6", "3", "2", "5"}, {"2", "3", "2", "5"}, {"2", "2", "2", "5"}, {"2", "3", "2", "7"}};
+        ArrayValueCalculator arrayValueCalculator = new ArrayValueCalculator();
+        try {
+            arrayValueCalculator.doCalc(array);
+        } catch (ArraySizeException e) {
+            e.printStackTrace();
+        } catch (ArrayDataException e) {
+            e.printStackTrace();
+        }
+    }
+}
