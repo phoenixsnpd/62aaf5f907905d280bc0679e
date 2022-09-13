96661a1c16628862c552ae3b6af2de20918f5555
phoenixsnpd <phoenix.snpd@gmail.com>
2022-09-13 12:41:57 +0300

    Corrected mistakes.

--- ua.ithillel.homework/src/ua/ithillel/lesson11/Main.java
+++ ua.ithillel.homework/src/ua/ithillel/lesson11/Main.java
@@ -37,7 +37,7 @@ public class Main {
         System.out.println(boxWithApple.getFruits().size());
     }
 
-    public static <T> List toList(T[] array) {
+    public static <T> List<T> toList(T[] array) {
         List<T> newList = new ArrayList<>();
         Collections.addAll(newList, array);
         return newList;
