a0dace4e58b45488534cbcd6a49238b4fb472018
phoenixsnpd <phoenix.snpd@gmail.com>
2022-09-19 22:15:52 +0300

    Corrected mistakes.

--- ua.ithillel.homework/src/ua/ithillel/lesson13/TestLambda.java
+++ ua.ithillel.homework/src/ua/ithillel/lesson13/TestLambda.java
@@ -7,40 +7,50 @@ import java.util.function.Supplier;
 
 public class TestLambda {
     public static void main(String[] args) {
-
-        // Testing Predicate
-
-        Predicate<String> voidString = s -> s.length() != 0;
-
-        System.out.println(voidString.test("Hello"));
-        System.out.println(voidString.test(""));
-        System.out.println(voidString.test("World"));
-        System.out.println();
-
-        Predicate<String> checkString = s -> s.endsWith("A") && s.startsWith("N") || s.startsWith("J");
-
-        System.out.println(checkString.test("John and NorA"));
-        System.out.println(checkString.test("NorA and John"));
-        System.out.println(checkString.test("Never go to BORA-BORA"));
-        System.out.println();
-
-        // Testing Consumer
-
         CargoBox cargoBox = new CargoBox(1000);
+
+        System.out.println(predicateTestOne("Hello"));
+        System.out.println(predicateTestOne(""));
+        System.out.println(predicateTestOne("World"));
+        System.out.println();
+
+        System.out.println(predicateTestTwo("John and NorA"));
+        System.out.println(predicateTestTwo("NorA and John"));
+        System.out.println(predicateTestTwo("Never go to BORA-BORA"));
+        System.out.println();
+
+        consumerTest(cargoBox);
+        System.out.println();
+
+        System.out.println(functionTest(100));
+        System.out.println();
+
+        System.out.println(suplierTest());
+    }
+
+    public static boolean predicateTestOne(String str) {
+        Predicate<String> voidString = s -> s.length() != 0;
+        return voidString.test(str);
+    }
+
+    public static boolean predicateTestTwo(String str) {
+        Predicate<String> checkString = s -> s.endsWith("A") && s.startsWith("N") || s.startsWith("J");
+        return checkString.test(str);
+    }
+
+    public static void consumerTest(CargoBox cargoBox) {
         Consumer<CargoBox>getCargoBox = box -> System.out.println("Отгрузили ящик с весом " + box.getWeight() + " kg.");
         Consumer<CargoBox> sendCargoBox = box -> System.out.println("Отправляем ящик с весом " + box.getWeight() + " kg.");
         getCargoBox.andThen(sendCargoBox).accept(cargoBox);
-        System.out.println();
+    }
 
-        // Testing Function
-
+    public static Double functionTest(Integer number) {
         Function<Integer, Double> function = Math::sqrt;
-        System.out.println(function.apply(100));
-        System.out.println();
+        return function.apply(number);
+    }
 
-        // Testing Supplier
-
+    public static Integer suplierTest() {
         Supplier<Integer> supplier = () -> (int) (Math.random() * 11);
-            System.out.println(supplier.get());
+        return supplier.get();
     }
 }
