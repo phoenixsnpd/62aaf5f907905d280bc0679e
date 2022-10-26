ae909bf8a7cd281a45d8204b099a6bd324e5ace3
phoenixsnpd <phoenix.snpd@gmail.com>
2022-10-27 00:08:37 +0300

    Homework 18 corrected

--- ua.ithillel.homework/src/ua/ithillel/homework18/Main.java
+++ ua.ithillel.homework/src/ua/ithillel/homework18/Main.java
@@ -2,19 +2,40 @@ package ua.ithillel.homework18;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
 public class Main {
     public static void main(String[] args) {
-        PetrolStation fuelStation = new PetrolStation(1000.0);
+        PetrolStation fuelStation = new PetrolStation(90.0);
+/*
+        Semaphore sem = new Semaphore(3);
+        PetrolStation fuelStation = new PetrolStation(90);
 
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        Thread thread1 = new Thread(() -> {System.out.println("поток 1 начал работу"); fuelStation.doRefuel(30); System.out.println("поток 1 закончил работу");});
+        Thread thread2 = new Thread(() -> {System.out.println("поток 2 начал работу"); fuelStation.doRefuel(30); System.out.println("поток 2 закончил работу");});
+        Thread thread3 = new Thread(() -> {System.out.println("поток 3 начал работу"); fuelStation.doRefuel(30); System.out.println("поток 3 закончил работу");});
+        Thread thread4 = new Thread(() -> {System.out.println("поток 4 начал работу"); fuelStation.doRefuel(10); System.out.println("поток 4 закончил работу");});
+        Thread thread5 = new Thread(() -> {System.out.println("поток 5 начал работу"); fuelStation.doRefuel(20); System.out.println("поток 5 закончил работу");});
+        Thread thread6 = new Thread(() -> {System.out.println("поток 6 начал работу"); fuelStation.doRefuel(30); System.out.println("поток 6 закончил работу");});
 
-        executorService.submit(fuelStation.doRefuel(20));
-        executorService.submit(fuelStation.doRefuel(30));
-        executorService.submit(fuelStation.doRefuel(15));
-        executorService.submit(fuelStation.doRefuel(5));
-        executorService.submit(fuelStation.doRefuel(5));
-        executorService.submit(fuelStation.doRefuel(80));
+
+
+            thread1.start();
+            thread2.start();
+            thread3.start();
+            thread4.start();
+            thread5.start();
+            thread6.start();
+
+*/
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        executorService.submit(() -> fuelStation.doRefuel(30));
+        executorService.submit(() -> fuelStation.doRefuel(30));
+        executorService.submit(() -> fuelStation.doRefuel(30));
+        executorService.submit(() -> fuelStation.doRefuel(10));
+        executorService.submit(() -> fuelStation.doRefuel(20));
+        executorService.submit(() -> fuelStation.doRefuel(30));
 
         executorService.shutdown();
     }
--- ua.ithillel.homework/src/ua/ithillel/homework18/PetrolStation.java
+++ ua.ithillel.homework/src/ua/ithillel/homework18/PetrolStation.java
@@ -1,28 +1,34 @@
 package ua.ithillel.homework18;
 
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
 
 public class PetrolStation {
-    private double amount;
+    private AtomicReference<Double> amount = new AtomicReference<>(0.0);
     private Semaphore semaphore = new Semaphore(3);
 
-    public PetrolStation(double amount) {
-        this.amount = amount;
+    public PetrolStation(Double amount) {
+        this.amount.set(amount);
     }
 
-    public Runnable doRefuel(double fuel) {
+    public synchronized void doRefuel(Integer fuel) {
+        try {
+            semaphore.acquire();
+        if (amount.get() < fuel) {
+            System.out.println("Not enough fuel");
+            return;
+        }
         int seconds = (int) (Math.random() * 8) + 3;
-        return () -> {
-            try {
-                semaphore.acquire();
-                TimeUnit.SECONDS.sleep(seconds);
-                amount = amount - fuel;
-                System.out.println(amount);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } finally {
-                semaphore.release();
-            }
-        };
+            TimeUnit.SECONDS.sleep(seconds);
+            amount.set(amount.get() - fuel);        //amount = amount - fuel;
+            System.out.println(amount);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            semaphore.release();
+        }
     }
 }
+
