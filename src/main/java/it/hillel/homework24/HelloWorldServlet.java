024e9eaf5c36d325516ced65acf0e722aeca28d6
phoenixsnpd <phoenix.snpd@gmail.com>
2022-12-02 12:48:14 +0200

    Homework-24

---
+++ src/main/java/it/hillel/homework24/HelloWorldServlet.java
@@ -0,0 +1,21 @@
+package it.hillel.homework24;
+
+import lombok.SneakyThrows;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+
+public class HelloWorldServlet extends HttpServlet {
+    @Override
+    @SneakyThrows
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        PrintWriter writer = resp.getWriter();
+        writer.println("Hello World!");
+        writer.close();
+    }
+}
