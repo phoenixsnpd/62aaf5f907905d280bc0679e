cbd25acb87164eababfd97abc8977258fdcc9b99
denyspolukhin <phoenix.snpd@gmail.com>
2023-01-20 20:39:31 +0200

    Homework-31

--- src/main/java/ua/ithillel/homework31/HibernateApplication.java
+++ src/main/java/ua/ithillel/homework31/HibernateApplication.java
@@ -8,11 +8,21 @@ public class HibernateApplication {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext("ua.ithillel.homework31");
         StudentDao studentDao = context.getBean(StudentDao.class);
-        Student student = new Student();
-        student.setId(1);
-        student.setName("Denys");
-        student.setEmail("phoenix.snpd@gmail.com");
+        Student student1 = new Student("Denys", "phoenix.snpd@gmail.com");
+        Student student2 = new Student("Jack", "Jack@gmail.com");
+        Student student3 = new Student("Rykard", "Rykard@gmail.com");
+        Student student4 = new Student("Updated person", "update@gmail.com");
 
-        studentDao.save(student);
+        studentDao.save(student1);
+        studentDao.save(student2);
+        studentDao.save(student3);
+
+        System.out.println(studentDao.getStudentByID(3));
+        System.out.println(studentDao.getAllStudents());
+
+        studentDao.update(3, student4);
+        System.out.println(studentDao.getStudentByID(3));
+
+        studentDao.delete(3);
     }
 }
