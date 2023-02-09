c65508a4aeac665df647bc549eb2213efcbd90ee
denyspolukhin <phoenix.snpd@gmail.com>
2023-02-09 13:24:17 +0200

    Homework-36. Corrected.

--- src/main/java/ua/ithillel/homework32/config/SecurityConfig.java
+++ src/main/java/ua/ithillel/homework32/config/SecurityConfig.java
@@ -4,35 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import ua.ithillel.homework32.services.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager(PasswordEncoder passwordEncoder) {
-        UserDetails user = User.withUsername("user")
-                .password(passwordEncoder.encode("12345"))
-                .roles("USER")
-                .build();
+    private final UserDetailsServiceImpl userDetailsService;
 
-        UserDetails admin = User.withUsername("admin")
-                .password(passwordEncoder.encode("12345"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
+    public SecurityConfig(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
     }
 
    @Bean
    public PasswordEncoder passwordEncoder() {
-       return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+       return NoOpPasswordEncoder.getInstance();
    }
 
     @Bean
@@ -41,7 +30,7 @@ public class SecurityConfig {
                 .cors().disable()
                 .csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/orders/**").hasRole("ADMIN")
+                .requestMatchers("/products**").hasRole("USER")
                 .anyRequest().permitAll()
                 .and()
                 .httpBasic();
